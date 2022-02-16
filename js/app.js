6d374cdf3fea003a01cf8412ebeb0b57021dc727
Deceit-87 <manuelfreund318@gmail.com>
2022-02-16 17:03:56 +0100

    controllo palindroma

--- js/app.js
+++ js/app.js
@@ -4,3 +4,41 @@
 // la parola inserita è palindroma -->
 
 
+
+
+
+function invertiParola (parola){
+
+const chars = parola.split('');
+
+console.log(chars)
+const rejoin = chars.reverse().join('');
+console.log(rejoin)
+
+return rejoin; 
+}
+function ePalindroma(parola){
+
+   
+
+    if(parola === invertiParola(parola)){
+        return true;
+
+    }
+    else {
+        return false ;
+    }
+}
+const parola = prompt('inserisci una parola')
+console.log(parola)
+
+invertiParola(parola);
+
+
+if(ePalindroma(parola)){
+    alert('la parola è palindroma')
+
+}
+else{
+    alert('la parola non è palindroma')
+}
