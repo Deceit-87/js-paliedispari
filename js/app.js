eced2ec1feddf97bc9531bc06384886c558a13b8
Deceit-87 <manuelfreund318@gmail.com>
2022-02-16 17:33:20 +0100

    pari e dispari

--- js/app.js
+++ js/app.js
@@ -5,40 +5,88 @@
 
 
 
+                //  ES PALINDROMA
 
+// function invertiParola (parola){
 
-function invertiParola (parola){
+// const chars = parola.split('');
 
-const chars = parola.split('');
+// console.log(chars)
+// const rejoin = chars.reverse().join('');
+// console.log(rejoin)
 
-console.log(chars)
-const rejoin = chars.reverse().join('');
-console.log(rejoin)
-
-return rejoin; 
-}
-function ePalindroma(parola){
+// return rejoin; 
+// }
+// function ePalindroma(parola){
 
    
 
-    if(parola === invertiParola(parola)){
-        return true;
+//     if(parola === invertiParola(parola)){
+//         return true;
 
-    }
-    else {
-        return false ;
-    }
+//     }
+//     else {
+//         return false ;
+//     }
+// }
+// const parola = prompt('inserisci una parola')
+// console.log(parola)
+
+// invertiParola(parola);
+
+
+// if(ePalindroma(parola)){
+//     alert('la parola è palindroma')
+
+// }
+// else{
+//     alert('la parola non è palindroma')
+// }
+
+
+
+
+                        // ES PARI E DISPARI
+
+
+
+function getRandomInt(min,max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min);
+
 }
-const parola = prompt('inserisci una parola')
-console.log(parola)
-
-invertiParola(parola);
 
 
-if(ePalindroma(parola)){
-    alert('la parola è palindroma')
+function ePari(numero){
+
+    console.log(numero)
+    if(numero % 2 === 0){
+    return true
+}
+
+    return false
+}
+
+const scelta = prompt('scegli pari o dispari');
+
+const uNumero = parseInt(prompt('Scegli un numero da 1 a 5')); 
+const pcNumero = getRandomInt(1,5);
+const risultato = uNumero + pcNumero;
+
+if (
+    ( scelta === 'pari' && ePari( risultato) ) || 
+( scelta === 'dispari' && !ePari( risultato) )
+ ) {
+     alert('hai vinto')
 
 }
 else{
-    alert('la parola non è palindroma')
+    alert('hai perso')
 }
+
+
+
+
+
+
